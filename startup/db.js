dac5d303243b0b0c9820f13df08316c3a901d584
fizzysuleman <fizzysuleman@gmail.com>
2019-06-22 00:32:29 +0100

    Done

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,9 @@
 const winston=require('winston')
 const mongoose=require('mongoose')
+const config=require('config')
 module.exports = function () {
     //connecting to our local mongodb 
-    mongoose.connect('mongodb://localhost/slicky', { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: true })
+    let db=config.get('slick_db')
+    mongoose.connect(db, { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: true })
         .then(() => winston.info('Connected to mongo'))
 }

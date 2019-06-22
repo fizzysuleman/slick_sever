6aa6058bcbed615cab34b5314fd0c7785813cf4d
fizzysuleman <fizzysuleman@gmail.com>
2019-06-22 08:33:56 +0100

    22June

--- startup/db.js
+++ startup/db.js
@@ -5,5 +5,5 @@ module.exports = function () {
     //connecting to our local mongodb 
     let db=config.get('db')
     mongoose.connect(db, { useCreateIndex: true, useNewUrlParser: true, useFindAndModify: true })
-        .then(() => winston.info('Connected to mongo'))
+        .then(() => winston.info(`Connected to  ${db} `))
 }
--- startup/logging.js
+++ startup/logging.js
@@ -1,5 +1,5 @@
 const winston = require('winston')
-require('winston-mongodb')
+// require('winston-mongodb')
 require('express-async-errors')
 const config=require('config')
 

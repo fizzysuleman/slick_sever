cf029240639eb85a2a5baa6c0c2c060d8a469bb6
fizzysuleman <fizzysuleman@gmail.com>
2019-08-12 09:52:02 +0100

    Validating object id

--- routes/itemPost.js
+++ routes/itemPost.js
@@ -6,10 +6,16 @@ const router = express.Router();
 
 //getting the token details by the id and return the response to the server
 router.get('/:postId',auth,async(req, res) => {
-  const post= await Post.findById(req.params.postId)
+Post.findById(req.params.postId)
+.then(result => {
+  res.send(result)
+})
+.catch(err => {
+  // When the ID isn't valid, it shows up as an error
+  res.status(404).send(err.message)
+})
 
 
-  res.send(post)
 });
 
 

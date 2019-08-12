732960e5b7724ecf148dd1baa07cc3279ca24232
fizzysuleman <fizzysuleman@gmail.com>
2019-08-12 08:45:06 +0100

    c deep

--- routes/deeplink.js
+++ routes/deeplink.js
@@ -5,7 +5,7 @@ var deeplink = require('node-deeplink');
  
 router.get('/',
   deeplink({
-    fallback: 'www.google.com',
+    fallback: 'https://play.google.com/store/apps/details?id=com.slick.slick',
     android_package_name: 'com.slick.slick',
     // ios_store_link:
     //   'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4'

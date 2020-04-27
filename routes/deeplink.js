e275bd925202a43359d8d34ed3b17977e227d5f8
fizzysuleman <fizzysuleman@gmail.com>
2020-04-27 18:55:38 -0300

    deep

--- routes/deeplink.js
+++ routes/deeplink.js
@@ -7,8 +7,8 @@ router.get('/',
   deeplink({
     fallback: 'https://play.google.com/store/apps/details?id=com.slick.slick',
     android_package_name: 'com.slick.slick',
-    // ios_store_link:
-    //   'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4'
+     ios_store_link:
+       'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4'
   })
 );
 

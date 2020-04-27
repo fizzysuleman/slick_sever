a302bb55dea0d292a22862d47458fc869356796d
fizzysuleman <fizzysuleman@gmail.com>
2020-04-27 19:20:11 -0300

    deeplink

--- routes/deeplink.js
+++ routes/deeplink.js
@@ -8,7 +8,7 @@ router.get('/',
     fallback: 'https://play.google.com/store/apps/details?id=com.slick.slick',
     android_package_name: 'com.slick.slick',
      ios_store_link:
-       'https://itunes.apple.com/us/app/cups-unlimited-coffee/id556462755?mt=8&uo=4'
+       'https://apps.apple.com/ca/app/slick-buy-and-sell/id1510237901'
   })
 );
 
